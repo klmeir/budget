09226c69c13324c8c6301eaaa020b764c0b0f254
klmeir <caalmeir@gmail.com>
2020-05-27 11:38:48 -0500

    Add state budget

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Question = () => {
+
+    const [quantity, setQuantity] = useState(0);
+
+    const defineBudget = e => {
+        setQuantity( parseInt(e.target.value, 10) );
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form
+                onSubmit={handleSubmit}
+            >
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={defineBudget}
                 />
 
                 <input
