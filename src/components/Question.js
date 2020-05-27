7850c071a73e1282135d5beba75220035f6d78bd
klmeir <caalmeir@gmail.com>
2020-05-27 12:08:09 -0500

    Defining budget and remaining

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,50 +1,49 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
 
-const Question = () => {
+const Question = ({ setBudget, setRemaining }) => {
+  const [quantity, setQuantity] = useState(0);
+  const [error, setError] = useState(false);
 
-    const [quantity, setQuantity] = useState(0);
-    const [error, setError] = useState(false);
+  const defineBudget = (e) => {
+    setQuantity(parseInt(e.target.value, 10));
+  };
 
-    const defineBudget = e => {
-        setQuantity( parseInt(e.target.value, 10) );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (quantity < 1 || isNaN(quantity)) {
+      setError(true);
+      return;
     }
 
-    const handleSubmit = e => {
-        e.preventDefault();
+    setError(false);
+    setBudget(quantity);
+    setRemaining(quantity);
+  };
 
-        if(quantity < 1 || isNaN(quantity)){
-            setError(true);
-            return;
-        }
+  return (
+    <Fragment>
+      <h2>Coloca tu presupuesto</h2>
 
-        setError(false);
-    }
+      {error ? <Error message="El Presupuesto es Incorrecto" /> : null}
 
-    return ( 
-        <Fragment>
-            <h2>Coloca tu presupuesto</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto"
+          onChange={defineBudget}
+        />
 
-            { error ? <Error message="El Presupuesto es Incorrecto" /> : null}
-
-            <form
-                onSubmit={handleSubmit}
-            >
-                <input 
-                    type="number"
-                    className="u-full-width"
-                    placeholder="Coloca tu presupuesto"
-                    onChange={defineBudget}
-                />
-
-                <input
-                    type="submit"
-                    className="button-primary u-full-width"
-                    value="Definir Presupuesto"
-                />
-            </form>
-        </Fragment>
-    );
-}
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir Presupuesto"
+        />
+      </form>
+    </Fragment>
+  );
+};
  
 export default Question;
