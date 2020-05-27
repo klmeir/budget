591feb2c77543608e2b148169823a32a504ef778
klmeir <caalmeir@gmail.com>
2020-05-27 11:43:38 -0500

    Add validation to form

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,6 +3,7 @@ import React, { Fragment, useState } from 'react';
 const Question = () => {
 
     const [quantity, setQuantity] = useState(0);
+    const [error, setError] = useState(false);
 
     const defineBudget = e => {
         setQuantity( parseInt(e.target.value, 10) );
@@ -10,11 +11,20 @@ const Question = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        if(quantity < 1 || isNaN(quantity)){
+            setError(true);
+            return;
+        }
+
+        setError(false);
     }
 
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
+
+            { error ? '' : null}
 
             <form
                 onSubmit={handleSubmit}
